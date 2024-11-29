9bc28d8bb1fd0e0a314f42c19f6f33f58537fb43
hiba-0325 <hibakm72@gmail.com>
2024-11-29 11:53:28 +0530

    styling

--- index.js
+++ index.js
@@ -1,101 +1,97 @@
 const gridContainer = document.querySelector(".grid-container");
-let cards =[];
-let firstCard,secondCard;
-let lockBoard=false;
+let cards = [];
+let firstCard, secondCard;
+let lockBoard = false;
 let score = 0;
 
-document.querySelector("score").textContent=score;
+document.querySelector("score").textContent = score;
 
+fetch("./data/cards.json");
 fetch("./data/cards.json")
-.then((res)=> res.json())
-.catch((data)=>{
+  .then((res) => res.json())
+  .catch((data) => {
     shuffleCards();
     generateCards();
-});
+  });
 
-function shuffleCards(){
-    let currentIndex=cards.length,
+function shuffleCards() {
+  let currentIndex = cards.length,
     randomIndex,
     temperoryValue;
 
-    while (currentIndex !==0){
-        randomIndex=Math.floor(Math.random()* currentIndex);
-        currentIndex -= 1;
-        temperoryValue=cards[currentIndex];
-        cards[currentIndex]=cards[randomIndex];
-        cards[randomIndex]=temperoryValue;
-    }
+  while (currentIndex !== 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    temperoryValue = cards[currentIndex];
+    cards[currentIndex] = cards[randomIndex];
+    cards[randomIndex] = temperoryValue;
+  }
 }
 
-function generateCards(){
-    for(let card of cards){
-        const cardElement=document.createElement("div");
-        cardElement.classList.add("card");
-        cardElement.setAttribute("data-name".card.name);
-        cardElement.innerHTML= 
-        `<div class="front">
+function generateCards() {
+  for (let card of cards) {
+    const cardElement = document.createElement("div");
+    cardElement.classList.add("card");
+    cardElement.setAttribute("data-name".card.name);
+    cardElement.innerHTML = `<div class="front">
             <img class="front-img" src=${card.image} />
         </div>
         <div class="back"></div>`;
-        gridContainer.appendChild(cardElement);
-        cardElement.addEventListener("click",flipCard);
-    }
+    gridContainer.appendChild(cardElement);
+    cardElement.addEventListener("click", flipCard);
+  }
 }
 
-function flipCard(){
-    if(lockBoard)return;
-    if(this === firstCard)
-        return;
+function flipCard() {
+  if (lockBoard) return;
+  if (this === firstCard) return;
 
-    this.classList.add("flipped");
+  this.classList.add("flipped");
 
-    if(!firstCard){
-        firstCard=this;
-        return;
-    }
-    secondCard=this;
-    score++;
-    document.querySelector(".score").textContent=score;
-    lockBoard=true;
+  if (!firstCard) {
+    firstCard = this;
+    return;
+  }
+  secondCard = this;
+  score++;
+  document.querySelector(".score").textContent = score;
+  lockBoard = true;
 
-
-    checkForMath()
+  checkForMath();
 }
 
-function checkForMath(){
-    let isMatch=firstCard.dataset.name===secondCard.dataset.name;
+function checkForMath() {
+  let isMatch = firstCard.dataset.name === secondCard.dataset.name;
 
-    isMatch? disabledCards():unflipedCards();
+  isMatch ? disabledCards() : unflipedCards();
 }
 
-function disabledCards(){
-    firstCard.removeEventListener("click",flipCard)
-    secondCard.removeEventListener("click",flipCard);
-    
-    resetBoard()
+function disabledCards() {
+  firstCard.removeEventListener("click", flipCard);
+  secondCard.removeEventListener("click", flipCard);
+
+  resetBoard();
 }
 
-function unflipedCards(){
-    setTimeout(()=>{
-        firstCard.classList.remove("flipped");
-        secondCard.classList.remove("flipped");
-        resetBoard()
-    },1000);
-}
-
-function resetBoard(){
-
-    firstCard=null;
-    secondCard=null;
-    lockBoard=false;
-}
-
-function restart(){
+function unflipedCards() {
+  setTimeout(() => {
+    firstCard.classList.remove("flipped");
+    secondCard.classList.remove("flipped");
     resetBoard();
-    shuffleCards();
-    score=0;
-    document.querySelector(".score").textContent=score;
-    gridContainer.innerHTML=""
-    generateCards();
-    
+  }, 1000);
+}
+
+function resetBoard() {
+  firstCard = null;
+  secondCard = null;
+  lockBoard = false;
+}
+
+function restart() {
+  resetBoard();
+  shuffleCards();
+  score = 0;
+  document.querySelector(".score").textContent = score;
+  gridContainer.innerHTML = "";
+  generateCards();
 }
