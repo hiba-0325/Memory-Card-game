c9bba17f54ec0e73bc362114828182361c55dc69
hiba-0325 <hibakm72@gmail.com>
2024-11-29 11:01:16 +0530

    added cards asset and data

--- index.js
+++ index.js
@@ -6,4 +6,38 @@ let score = 0;
 
 document.querySelector("score").textContent=score;
 
-f
+fetch("./data/cards.json")
+.then((res)=> res.json())
+.catch((data)=>{
+    shuffleCards();
+    generateCards();
+});
+
+function shuffleCards(){
+    let currentIndex=cards.length,
+    randomIndex,
+    temperoryValue;
+
+    while (currentIndex !==0){
+        randomIndex=Math.floor(Math.random()* currentIndex);
+        currentIndex -= 1;
+        temperoryValue=cards[currentIndex];
+        cards[currentIndex]=cards[randomIndex];
+        cards[randomIndex]=temperoryValue;
+    }
+}
+
+function generateCards(){
+    for(let card of cards){
+        const cardElement=document.createElement("div");
+        cardElement.classList.add("card");
+        cardElement.setAttribute("data-name".card.name);
+        cardElement.innerHTML= 
+        `<div class="front">
+            <img class="front-img" src=${card.image} />
+        </div>
+        <div class="back"></div>`;
+        gridContainer.appendChild(cardElement);
+        cardElement.addEventListener("click",flipCard);
+    }
+}
